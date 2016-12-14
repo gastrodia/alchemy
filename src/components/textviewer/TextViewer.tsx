f47d8b0d6639b2943c217c7defb1989ede461a0e
nil <nil@sanwu.org>
2016-12-14 22:15:10 +0800

    contenteditable

--- src/components/textviewer/TextViewer.tsx
+++ src/components/textviewer/TextViewer.tsx
@@ -2,18 +2,29 @@ import * as React from "react";
 import {TextEntity} from '../../core/entity/TextEntity'
 import * as styles from './textviewer.css'
 
+ var ContentEditable = require("react-contenteditable");
+
 export class TextViewer extends React.Component<any, any> {
     get entity():TextEntity{
         return this.props.entity;
     };
+
+
+
+    handleChange(evt:any){
+      this.setState({html: evt.target.value});
+    }
+
     render() {
         return <div className={styles.textviewer}>
             <div className={styles.title}>
                 TextViewer
             </div>
-            <div className={styles.body} contenteditable="true">
-                {this.entity.text}
-            </div>
+             <ContentEditable
+                html="<b>Hello <i>World</i></b>" // innerHTML of the editable div
+                disabled={false}       // use true to disable edition
+                onChange={this.handleChange} // handle innerHTML change
+              />
         </div>;
     }
 }
