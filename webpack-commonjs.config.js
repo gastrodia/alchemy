8d16864158a1d60f213c5d03e4a59eb58c17b23a
nil <nil@sanwu.org>
2017-01-05 20:59:19 +0800

    commonjs

---
+++ webpack-commonjs.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var _ = require('lodash');
+
+// copy most settings from the default config, and then modify
+// the stuff that has changed
+
+var defaultconfig = require('./webpack.config.js');
+
+var commonjsconfig = _.cloneDeep(defaultconfig);
+_.assign(commonjsconfig, {
+  entry:      "./src/index.ts",
+  externals: [
+    "react",
+    /^react\/lib\/.+/  // any require that refers to internal react modules
+  ]
+});
+_.assign(commonjsconfig.output, {
+  path: path.join(__dirname, "es5"),
+  libraryTarget: "commonjs2",
+  library: "alchemy-framework",
+  filename: "alchemy-commonjs.js"
+});
+
+module.exports = commonjsconfig;
